b6db8d8ecce8dba6a8fa6687cc08d8250b4f3ff2
Herbert Marshall <herbert@herbmarshall.com>
2024-05-06 14:23:05 -0400

    UTIL-340 Add copyright

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#
+# This file is part of herbmarshall.com: standardPipe.library  ( hereinafter "standardPipe.library" ).
+#
+# standardPipe.library is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either version 2 of the License,
+# or (at your option) any later version.
+#
+# standardPipe.library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with standardPipe.library.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+
 set -e
 
 # Using non-cdlocal because may be ran by jenkins
